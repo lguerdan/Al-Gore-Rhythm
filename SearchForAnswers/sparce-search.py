88b7d4368609f77fc0daa5bb4de0850657854f97
Luke Guerdan <lguerdan@yahoo.com>
2016-08-03 13:50:51 -0500

    search sparce stuff & graphs

---
+++ SearchForAnswers/sparce-search.py
@@ -0,0 +1,11 @@
+ball = ["at", "", "", "", "ball", "", "", "car", "","", "dad", "", ""]
+
+def sparce_search(list, low, high, key):
+
+  while(low < high):
+    mid = (high + low)/2
+    mid_search  = 0
+    while arr[mid] == "" and (mid + midsearch) < = high and mid - mid_search >= low:
+      asdf
+
+print sparce_search(ball, 0, 12, 'ball')
